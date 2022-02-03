803aa2fd8b0927838e5777a1d02e875b2d4b6661
omega1x <omega1x@gmail.com>
2022-02-03 20:37:00 +0300

    Add ball described with difference equation

--- sim.go
+++ sim.go
@@ -12,14 +12,30 @@ func sim(t int) {
 			x[t][n] = 0.0 + vx[t][n]*float64(t)   // [pixel]
 			y[t][n] = 540.0 + vy[t][n]*float64(t) // [pixel]
 
-		case 9:
-			a := .002 // [pixel/frame^2]
+		case 1:
+			g := .002 // [pixel/frame^2]
 
 			vx[t][n] = 2.6            // [pixel/frame]
-			vy[t][n] = a * float64(t) // [pixel/frame]
+			vy[t][n] = g * float64(t) // [pixel/frame]
 
 			x[t][n] = 0.0 + vx[t][n]*float64(t)   // [pixel]
 			y[t][n] = 140.0 + vy[t][n]*float64(t) // [pixel]
+		case 2:
+			g := .003 // [pixel/frame^2]
+			switch t {
+			case 0:
+				vx[t][n] = 1.8 // [pixel/frame]
+				vy[t][n] = -.7 // [pixel/frame]
+
+				x[t][n] = 0.0   // [pixel]
+				y[t][n] = 400.0 // [pixel]
+			default:
+				vx[t][n] = vx[t-1][n]     // [pixel/frame]
+				vy[t][n] = vy[t-1][n] + g // [pixel/frame]
+
+				x[t][n] = x[t-1][n] + vx[t-1][n] // [pixel]
+				y[t][n] = y[t-1][n] + vy[t-1][n] // [pixel]
+			}
 		}
 	}
 }
